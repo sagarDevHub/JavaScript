8df8e7d3e1e486fe46c0a87fd66cab46cc666f8d
Sagar Gulla <gullasagar9933@gmail.com>
2024-06-22 14:13:45 +0000

    ARROW functions

---
+++ 02_Basics/arrowFunc.js
@@ -0,0 +1,8 @@
+const sum = (num1, num2) =>{
+    return num1+num2;
+}
+
+// implicit return () => () no need to write return statement
+// const sum = (num1, num2) => ( num1 + num2 );
+
+console.log(sum(2,2));
