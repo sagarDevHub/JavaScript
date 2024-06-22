4c5a41b44373e665f5c68fa5a3cf9ef6000bca7b
Sagar Gulla <gullasagar9933@gmail.com>
2024-06-22 13:31:05 +0000

    Object part two

---
+++ 01_Basics/object_Two.js
@@ -0,0 +1,44 @@
+const regularUser = {
+    email: "sagar@gmail.com",
+    fullname: {
+        firstName:"sagar",
+        lastName:"gulla"
+    }
+}
+
+console.log(regularUser.fullname.firstName);
+
+const obj1 = {
+    1:"A",
+    2:"B",
+    3:"C"
+}
+const obj2 = {
+    4:"D",
+    5:"E",
+    6:"F"
+}
+const obj3 = {
+    7:"G",
+    8:"H",
+    9:"I"
+}
+
+const obj4 = Object.assign(obj1, obj2)
+console.log(obj4);
+
+// combining three or more objects
+const obj5 = Object.assign({}, obj1, obj2, obj3);
+console.log(obj5);
+
+//taking all the keys
+console.log(Object.keys(obj1));
+
+//taking all the values
+console.log(Object.values(obj1));
+
+//taking all the entries
+console.log(Object.entries(obj1));
+
+// cheking condition
+console.log(regularUser.hasOwnProperty('name'));
